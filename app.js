10502bd69f9e9c1333d2333a9e55d6e9cbb9fc21
Ankur Prajapati <91510065+Ankur77720@users.noreply.github.com>
2022-10-12 18:45:34 +0530

    new buttons

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var music = new Audio("./music.mp3");
 var score = 0;
+var HighScore = 0;
 var clutter = "";
 for (var z = 0; z < 1225; z++) {
   clutter += `<div data-num="${2 * z}" class="square" id="s${2 * z}"></div>
@@ -32,6 +33,7 @@ function snak() {
   var foody = Math.floor(Math.random() * (49.5 - 1) + 1);
   document.querySelector(`#s${foodx * foody}`).style.backgroundColor = "green";
   window.addEventListener("click", (eve) => {
+    console.log(eve.target);
     switch (eve.target.id) {
       case "left":
         if (velx === 0) {
@@ -63,60 +65,81 @@ function snak() {
         break;
     }
   });
+  var ispaused = false;
+
   var funcy = setInterval(() => {
-    try {
-      if (window.innerHeight < window.innerWidth) {
-        document.querySelector(
-          `#s${len[len.length - 1].x}`
-        ).style.backgroundColor = "rgb(96, 207, 255)";
-        for (var k = len.length - 1; k > 0; k--) {
-          len[k].x = len[k - 1].x;
+    if (HighScore <= score) {
+      HighScore = score;
+    }
+    if (!ispaused) {
+      try {
+        if (window.innerHeight < window.innerWidth) {
+          document.querySelector(
+            `#s${len[len.length - 1].x}`
+          ).style.backgroundColor = "rgb(96, 207, 255)";
+          for (var k = len.length - 1; k > 0; k--) {
+            len[k].x = len[k - 1].x;
+          }
+          cury += vely;
+          nxtpos += velx;
+          document.querySelector(
+            `#s${curx * cury + nxtpos}`
+          ).style.backgroundColor = "rgb(99, 99, 99)";
+          len[0].x = curx * cury + nxtpos;
+          len.forEach((eve) => {
+            document.querySelector(`#s${eve.x}`).style.backgroundColor =
+              "rgb(99, 99, 99)";
+          });
+
+          document.querySelector("#score").textContent = `Score : ${score}`;
+          document.querySelector(
+            "#highScore"
+          ).textContent = `HighScore : ${HighScore}`;
+          document.querySelector(`#s${foodx * foody}`).style.backgroundColor =
+            "green";
+          if (len[0].x === foodx * foody) {
+            food.play();
+            len.push({ x: 0 });
+            foodx = Math.floor(Math.random() * 35.5);
+            foody = Math.floor(Math.random() * 49.5);
+            score++;
+          }
+          if (nxtpos < -35 && velx === -1) {
+            gameover.play();
+            gameOver();
+            throw "Game Over";
+          }
+          if (nxtpos > 34 && velx === 1) {
+            gameover.play();
+            gameOver();
+            throw "Game Over";
+          }
+        } else {
+          alert("Rotate your phone to play game !");
         }
-        cury += vely;
-        nxtpos += velx;
-        document.querySelector(
-          `#s${curx * cury + nxtpos}`
-        ).style.backgroundColor = "rgb(99, 99, 99)";
-        len[0].x = curx * cury + nxtpos;
-        len.forEach((eve) => {
-          document.querySelector(`#s${eve.x}`).style.backgroundColor =
-            "rgb(99, 99, 99)";
-        });
-        document.querySelector("#score").textContent = `Score : ${score}`;
-        document.querySelector(`#s${foodx * foody}`).style.backgroundColor =
-          "green";
-        if (len[0].x === foodx * foody) {
-          food.play();
-          len.push({ x: 0 });
-          foodx = Math.floor(Math.random() * 35.5);
-          foody = Math.floor(Math.random() * 49.5);
-          score++;
-        }
-        if (nxtpos < -35 && velx === -1) {
-          gameover.play();
-          gameOver();
-          throw "Game Over";
-        }
-        if (nxtpos > 34 && velx === 1) {
-          gameover.play();
-          gameOver();
-          throw "Game Over";
-        }
-      } else {
-        alert("Rotate your phone to play game !");
+      } catch (e) {
+        gameover.play();
+        clearInterval(funcy);
+        gameOver();
+        throw "Game Over";
       }
-    } catch (e) {
-      gameover.play();
-      clearInterval(funcy);
-      gameOver();
-      throw "Game Over";
     }
   }, 100);
-
+  function pausefun() {
+    if (ispaused) {
+      ispaused = false;
+    }
+  }
   window.onkeydown = function (event) {
     music.play();
     switch (event.keyCode) {
+      case 32:
+        ispaused = !ispaused;
+        break;
       case 37:
+      case 65:
+      case 100:
+        pausefun();
         if (velx === 0) {
           move.play();
           velx = -1;
@@ -124,6 +147,9 @@ function snak() {
         }
         break;
       case 38:
+      case 87:
+      case 104:
+        pausefun();
         if (vely === 0) {
           move.play();
           velx = 0;
@@ -131,6 +157,9 @@ function snak() {
         }
         break;
       case 39:
+      case 68:
+      case 102:
+        pausefun();
         if (velx === 0) {
           move.play();
           velx = 1;
@@ -138,10 +167,11 @@ function snak() {
         }
         break;
       case 40:
+      case 83:
+      case 98:
+        pausefun();
         if (vely != -2) {
           move.play();
-          console.log("down");
-          console.log(vely);
           velx = 0;
           vely = 2;
         }
@@ -152,7 +182,7 @@ function snak() {
 snak();
 document.querySelector("#restart").addEventListener("click", () => {
   music.pause();
-  score=0;
+  score = 0;
   for (var z = 0; z < 1225; z++) {
     clutter += `<div data-num="${2 * z}" class="square" id="s${2 * z}"></div>
     <div data-num="${2 * z + 1}" class="square2" id="s${2 * z + 1}"></div>`;
